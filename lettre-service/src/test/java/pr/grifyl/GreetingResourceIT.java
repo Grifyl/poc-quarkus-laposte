6cf958fd034a105688827b7a059f18760cdea0ba
Pierre REYNAUD <prnreynaud@gmail.com>
2024-05-27 18:48:12 +0200

    Création de lettre-service

---
+++ lettre-service/src/test/java/pr/grifyl/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package pr.grifyl;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
