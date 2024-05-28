56f4e2657bc9c1ec0e7fb4334ee7522bfd332c10
Pierre REYNAUD <prnreynaud@gmail.com>
2024-05-28 19:56:16 +0200

    Letter properties private to public

--- lettre-service/src/main/java/pr/grifyl/Letter.java
+++ lettre-service/src/main/java/pr/grifyl/Letter.java
@@ -8,40 +8,8 @@ import io.quarkus.mongodb.panache.common.MongoEntity;
 @MongoEntity(collection = "letters")
 public class Letter {
     @BsonId
-    private ObjectId id;
-    private String type;
-    private String status;
-    private String destination;
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
+    public ObjectId id;
+    public String type;
+    public String status;
+    public String destination;
 }
